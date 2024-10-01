963c7d7e7f460dcdf598ed064fa23199caf325ab
Canchehit <luisalejandroberreondoflorian@gmail.com>
2024-09-30 21:20:10 -0600

    Add files via upload

--- asignacion.java
+++ asignacion.java
@@ -1,26 +1,93 @@
 package Asignacion;
 
-import Modelo.*;
-import *;
+
 import java.util.Scanner;
 
 public class asignacion {
    public static void main(String[]args){ 
        Scanner sc = new Scanner(System.in);
-       System.out.println("seleccione el tipo de categoria ");
-       String categoria = 0;
-       // esta funcion nos ayuda para seleccionar el tipo de producto
-       
-               switch (categoria){
-                   case 1: categoria = "bebida carbonatada";
-                   System.out.println("ingrese la espesificación y sus caracteristicas ");
-                   break;
-                   case 2: categoria = "bebida energetica";
-                   System.out.println("ingrese la espesificación y sus caracteristicas ");
-                   break;
-                   default: categoria = "No existe esa categoria";
-                   break;
-               }
+       System.out.println("Ingrese el número 1 si desa crear un Nuevo producto:");
+       System.out.println("Ingrese el número 2 si desa cambiar algun producto:");
+       int num = 0;
+      if (num == 1){
+             System.out.println("selecciona la categoria de tu nuevo producto:");
+    
+             
+       int nuevo = 0;
+    switch (nuevo){
+          case 1:
+              System.out.println("Sodas carbonatadas");
+              break;
+              
+          case 2: 
+              System.out.println("Sodas");
+              
+              
+          case 3: 
+              System.out.println("Enregisantes");
+             
+              break;
+              default: 
+              System.out.println("No corresponde a ningun producto");
+              break;
+            }
+             System.out.println("Esta es la categoria que escojistes:"+nuevo);
+
+             
+             String caracteristica;
+             String Descripccion;
+     //Este conjutno es para que el usuario cree la descripccion      
+             System.out.println("ingresa su Caracteristica");
+           String caracteristica = sc.nextLine();
+      //Este conjutno es para que el usuario cree la descripccion
+             System.out.println("ingresa su Descripcción ");
+           String Descripccion = sc.nextLine();
+      
+    System.out.println("Producto creaado con exito  ");  
+      
+    
+      }else{ 
+          if (num == 2){
+          int producto = 0;
+          switch(producto){
+             
+              case 1:
+              System.out.println("Coca-Cola Original");
+              break;
+              case 2: 
+              System.out.println("Coca-Cola Zero");
+              break;
+              case 3: 
+              System.out.println("Coca-Cola Light");
+              break;
+              case 4: 
+              System.out.println("Sprite");
+              break;
+              case 5: 
+              System.out.println("Fanta");
+              break;
+              case 6: 
+              System.out.println("powerade");
+              break;
+              case 7: 
+              System.out.println("monster energy");
+              break;
+              default: 
+              System.out.println("No corresponde a ningun producto");
+              break;
+          }
+          System.out.println("El producto escogido es:"+ producto);
+          
+           String Ncaracteristica;
+           String NDescripccion;
+     //Este conjutno es para que el usuario cree la nueva caracterisrica     
+          System.out.println("ingresa la nueva Caracteristica");
+           String Ncaracteristica = sc.nextLine();
+     //Este conjutno es para que el usuario cree la nueva descripccion     
+          System.out.println("ingresa la nueva Descripccion");
+           String NDescripccion = sc.nextLine();
+          }
+      }
    }
     
     
