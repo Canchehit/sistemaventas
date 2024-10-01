3282c21128c81154d36b80bdb75cae03fcc3a186
Canchehit <luisalejandroberreondoflorian@gmail.com>
2024-10-01 17:35:51 -0600

    Add files via upload

---
+++ Registro.java
@@ -0,0 +1,47 @@
+
+public class Registro {
+    public static void main(String[]args){
+    System.out.println("Ingrese codigo");
+    
+    int CocaColaOriginal = 123;
+        int CocaColaZero = 456;
+        int Sprite = 789;
+        int CocaColaLight = 1011;
+        int monsterEnergy = 1213;
+        int powerade = 1415;
+     
+   // Esta funcion if nos sirve para seleccionar el producto por su codigo unico 
+    int codigo = 0;
+    if (codigo == 123){
+         System.out.println("CocaColaOriginal");
+         System.out.println("Ingresa la cantidad de productos");
+    }else{
+        if (codigo == 456){
+        System.out.println("CocaColaZero");
+        System.out.println("Ingresa la cantidad de productos");
+        }else{
+        if (codigo == 789){
+        System.out.println("Sprite");
+        System.out.println("Ingresa la cantidad de productos");
+        }else{
+        if (codigo == 1011){
+        System.out.println("CocaColaLight");
+        System.out.println("Ingresa la cantidad de productos");
+        }else{
+        if (codigo == 1213){
+        System.out.println("monsterEnergy");
+        System.out.println("Ingresa la cantidad de productos");
+        }else{
+         if (codigo == 1415){
+        System.out.println("powerade");
+        System.out.println("Ingresa la cantidad de productos");      
+                
+        }       
+        }
+        }
+        }
+        }
+      
+      
+    }
+}
