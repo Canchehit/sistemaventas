b664fb89c3c0986d8f345b870dd929cb7074f82e
Canchehit <luisalejandroberreondoflorian@gmail.com>
2024-10-02 20:48:11 -0600

    Add files via upload

---
+++ Historial.java
@@ -0,0 +1,75 @@
+
+import java.util.Scanner;
+
+public class Historial {
+  
+     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("ingresa el codigo del producto");
+    
+    //Esta funcion nos sirve para seleccionar el producto
+  int producto = 0;
+  char usuario = 0;
+              
+          switch(producto){
+             
+              case 1:
+              System.out.println("Coca-Cola Original");
+              System.out.println("cantida de stock disponibles:");
+            System.out.println("2000");
+             System.out.println("El producto escogido es:"+ producto);
+                    System.out.println( "La persona que lo realizo  es:"+usuario);
+              break;
+              case 2: 
+              System.out.println("Coca-Cola Zero");
+               System.out.println("cantida de stock:");
+                System.out.println("500"); 
+                 System.out.println("El producto escogido es:"+ producto);
+                    System.out.println( "La persona que lo realizo  es:"+usuario);
+              break;
+              case 3: 
+              System.out.println("Coca-Cola Light");
+              System.out.println("cantida de stock:");
+                        System.out.println("600");
+                         System.out.println("El producto escogido es:"+ producto);
+                    System.out.println( "La persona que lo realizo  es:"+usuario);
+              break;
+              case 4: 
+              System.out.println("Sprite");
+              System.out.println("cantida de stock:");
+                    System.out.println("1000");
+                     System.out.println("El producto escogido es:"+ producto);
+                    System.out.println( "La persona que lo realizo  es:"+usuario);
+              break;
+              case 5: 
+              System.out.println("powerade");
+              System.out.println("cantida de stock:");
+                             System.out.println("1000");
+                              System.out.println("El producto escogido es:"+ producto);
+                    System.out.println( "La persona que lo realizo  es:"+usuario);
+              break;
+              case 6: 
+              System.out.println("monster energy");
+               System.out.println("cantida de stock:");
+                            System.out.println("1000");
+                             System.out.println("El producto escogido es:"+ producto);
+                    System.out.println( "La persona que lo realizo  es:"+usuario);
+              break;
+              default: 
+              System.out.println("No corresponde a ningun producto");
+              break;
+          }
+    
+
+          
+          
+          
+          
+          
+          
+          
+        }
+    
+  
+    
+}
